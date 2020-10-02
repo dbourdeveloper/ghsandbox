3ad1be72d149598d56f553da90a67ccaf3b0b276
david bour <dbour.developer@gmail.com>
2020-10-02 11:32:57 -0700

    increase

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,5 +5,6 @@ mkdir ~/.ssh
 echo "$ANSIBLE_PRIVATE_SSH_KEY" > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
+export ANSIBLE_CONFIG="ansible/ansible.cfg"
 # ansible-playbook -i deployment/ansible/hosts --extra-vars "webservers=$DEPLOY_ENV docker_image_hash=$DOCKER_IMG_HASH git_timestamp=$GIT_COMMIT_TIMESTAMP" deployment/ansible/playbooks/deploy.yml --vault-password-file ~/.vault_pass.txt
-ansible-playbook -i ansible/hosts ansible/playbooks/play.yml
+ansible-playbook ansible/playbooks/play.yml
